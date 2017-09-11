68e2f4daee6a3c7a152ec144655a4baae2f66c62
selenium-sso-user-payment <30900427+selenium-sso-user-payment@users.noreply.github.com>
2017-09-11 09:27:57 +0300

    Add files via upload

---
+++ source/Nokia Qt.ts
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru" sourcelanguage="en">
+<context>
+    <name>ClientInfo</name>
+    <message>
+        <source>Software information</source>
+        <translation type="obsolete">Информация о приложении</translation>
+    </message>
+    <message>
+        <source>Software</source>
+        <translation>Приложение</translation>
+    </message>
+    <message>
+        <source>Software Version</source>
+        <translation>Версия приложения</translation>
+    </message>
+    <message>
+        <source>OS</source>
+        <translation type="obsolete">ОС</translation>
+    </message>
+    <message>
+        <source>OS Version</source>
+        <translation>Версия ОС</translation>
+    </message>
+    <message>
+        <source>Client Information</source>
+        <translation>Клиентская информация</translation>
+    </message>
+    <message>
+        <source>Allows to send and receive information about the version of the application, the local time and the last activity of contact</source>
+        <translation>Позволяет отправлять и получать информацию о версии приложения, локальном времени и последней активности контакта</translation>
+    </message>
+    <message>
+        <source>Supports the exchanging of the information about the application version</source>
+        <translation>Поддерживает обмен информацией о версии приложения</translation>
+    </message>
+</context>
+</TS>
