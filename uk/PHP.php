189995e20fa4cc8f00bcd64aa4bdbb07eaae8457
selenium-sso-user-payment <30900427+selenium-sso-user-payment@users.noreply.github.com>
2017-09-13 11:44:49 +0300

    New translations PHP.php (Ukrainian)

---
+++ uk/PHP.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+
+	'home' => 'Home',
+	'login' => 'Login',
+  'speakers' => 'Speakers',
+  'congregations' => 'Congregations',
+  'noCongregations' => 'There\'s no congregations.',
+  'family' => array(
+          'father' => 'John',
+          'mother' => 'Susan',
+          'children'  => array(
+               'son' => 'Jimmy',
+          ),
+  ),
+  'plural' => 'one apple|a million apples'
+);
