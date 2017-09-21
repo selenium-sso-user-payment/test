92f2f4ac67a4e6fdb70ea0a9a1c9cbe77fcd44ef
selenium-sso-user-payment <30900427+selenium-sso-user-payment@users.noreply.github.com>
2017-09-21 09:06:32 -0300

    New translations Nokia Qt.ts (Acholi)

---
+++ ach/Nokia Qt.ts
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ach" sourcelanguage="en">
+  <context>
+    <name>ClientInfo</name>
+    <message>
+      <source>Software information</source>
+      <translation type="unfinished">Software information</translation>
+    </message>
+    <message>
+      <source>Software</source>
+      <translation type="unfinished">Software</translation>
+    </message>
+    <message>
+      <source>Software Version</source>
+      <translation type="unfinished">Software Version</translation>
+    </message>
+    <message>
+      <source>OS</source>
+      <translation type="unfinished">OS</translation>
+    </message>
+    <message>
+      <source>OS Version</source>
+      <translation type="unfinished">OS Version</translation>
+    </message>
+    <message>
+      <source>Client Information</source>
+      <translation type="unfinished">Client Information</translation>
+    </message>
+    <message>
+      <source>Allows to send and receive information about the version of the application, the local time and the last activity of contact</source>
+      <translation type="unfinished">Allows to send and receive information about the version of the application, the local time and the last activity of contact</translation>
+    </message>
+    <message>
+      <source>Supports the exchanging of the information about the application version</source>
+      <translation type="unfinished">Supports the exchanging of the information about the application version</translation>
+    </message>
+  </context>
+</TS>
